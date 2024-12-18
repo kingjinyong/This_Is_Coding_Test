e32e58eb2d58c2efa5cbe8b97e867703781801ac
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-18 10:45:24 +0900

    2024/12/18

--- Greedy/example_3-4.py
+++ Greedy/example_3-4.py
@@ -1,35 +1,49 @@
+# 내 답안
 n, k = map(int, input().split())
 
-count = 0
-while n >= k:               # n이 k 이상이라면 k로 계속 나누기
-    if n % k != 0:          # 나누어 떨어지지 않는다면?
-        n -= 1              # 1 빼주고
-        count += 1          # 횟수 업
-    n //= k                 # 나누어 떨어지면 나누기
-    count += 1
-# 자 이제 n이 k보다 작아졌어
-while n > 1:            # 남은 수 1씩 뺴주면서 횟수 업
-    n -= 1
-    count += 1
-print(count)
-#---------- 단순하게 푸는 방법이었대... ----------
+cnt = 0
 
-n, k = map(int, input().split()) # 17 4로 주어졌다면?
-count = 0
+while n != 1:
+    cnt += 1
+    if n % k == 0:
+        n /= k
+    else:
+        n -= 1
+print(cnt)
+
+#책 답안1 (단순)
+n, k = map(int, input().split())
+result = 0
+
+while n >= k:
+    # N이 K로 나누어 떨어지지 않는다면 N에서 1씩 빼기
+    while n % k != 0:
+        n -= 1
+        result += 1
+
+    n //= k
+    result += 1
+
+while n > 1:
+    n -= 1
+    result += 1
+
+print(result)
+
+#책 답안 (효율?)
+n, k = map(int, input().split())
+result = 0
 
 while True:
-    # n == k 로 나누어떨어지는 수 가 될 때까지 1씩 빼기 -> 수식으로 구해버림
-    target = (n//k)*k   # target = (17//4) * 4 -> target = 4*4 = 16
-    count += (n - target)   # count += 17 - 16 -> count += 1
-    n = target  # n = 16
-    # n이 k보다 작을 때(더 이상 나눌 쑤 없을 때) 반복문 탈출
+    target = (n // k) * k
+    result += (n - target)
+    n = target
+
     if n < k:
         break
-    # k로 나누기
-    count += 1
-    n //= k     # n = 16//4 -> n = 4
-    # -> 이후 반복
 
-# 마지막으로 남은 수에 대하여 1씩 빼기 -> 이거도 수식으로 해버림
-count += (n - 1)
-print(count)
+    result += 1
+    n //= k
+
+result += (n - 1)
+print(result)
