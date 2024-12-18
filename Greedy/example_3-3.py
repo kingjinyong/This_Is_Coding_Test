fccf8c6da56c7980707871bd4c511009bdfdbb31
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-18 10:27:37 +0900

    2024/12/18

--- Greedy/example_3-3.py
+++ Greedy/example_3-3.py
@@ -1,7 +1,32 @@
+# 내 답안
 n, m = map(int, input().split())
-min_v = 10001
-mins = []                                   # 각 행에서 가장 작은 수를 저장 할 리스트
+
+ary = []
 for i in range(n):
-    ary = list(map(int, input().split()))
-    mins.append(min(ary))                   # 리스트에서 가장 작은 수 리스트에 추가
-print(max(mins))                            # 작은 수들을 모아놓은 리스트에서 가장 큰 수 출력
+    ary.append(min(map(int, input().split())))
+print(max(ary))
+
+# 책 답안1(min() 함수 이용)
+n, m = map(int, input().split())
+
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = min(data)
+    result = max(result, min_value)
+print(result)
+
+# 책 답안2(2중 반복문 구조 사용)
+n, m = map(int, input().split())
+
+result = 0
+for i in range(n):
+    data = list(map(int, input().split()))
+
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
+    result = max(result, min_value)
+
+print(result)
