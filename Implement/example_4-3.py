7ba5917736e9808c3db3d8fd37d00ef05f34dbb8
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-19 16:30:57 +0900

    2024/12/19

--- Implement/example_4-3.py
+++ Implement/example_4-3.py
@@ -1,20 +1,36 @@
-knite = input()
+# 내 답안
+locate = list(input())
+x, y = int(ord(locate[0]) - ord('a') + 1) - 1, int(locate[1]) - 1
 
-row = int(knite[1])
-column = ord(knite[0])
+dx = [1, -1, 1, -1, 2, -2, 2, -2]
+dy = [2, 2, -2, -2, 1, 1, -1, -1]
 
-ways = [(-1, -2), (-1, 2), (1, -2), (1, 2), (-2, -1), (-2, 1), (2, -1), (2, 1)]
+cnt = 0
+# 8가지 경우로 확인해주기
+for i in range(8):
+    nx = x + dx[i]
+    ny = y + dy[i]
 
-possible_count = 0
-# ord 사용하기
+    if 0 <= nx < 8 and 0 <= ny < 8:
+        cnt += 1
 
-for i in ways:
-    x, y = 0, 0
-    x = row + i[1]
-    y = column + i[0]
+print(cnt)
 
-    if x < 1 or x > 8 or y < ord('a') or y > ord('h'):
-        continue
-    possible_count += 1
-print(possible_count)
+# 책 답안
+input_data = input()
 
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+
+# 8가지 방향
+steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+
+result = 0
+for step in steps:
+    nr = row + step[0]
+    nc = column + step[1]
+
+    # 가능하면 result 증가
+    if 1 <= nr <= 8 and 1 <= nc <= 8:
+        result += 1
+print(result)
